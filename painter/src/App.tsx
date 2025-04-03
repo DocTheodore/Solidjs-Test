4fa81cad06bbbcf87dd9bf7440c4701a6abbe901
Thiago Moreira Theodoro <thiagomoreiraf273@gmail.com>
2025-04-03 18:01:51 -0300

    reorganização do código

--- painter/src/App.tsx
+++ painter/src/App.tsx
@@ -1,102 +1,91 @@
-import { createSignal, For } from 'solid-js'
-import './index.css'
+import { createSignal, For } from 'solid-js';
+import './index.css';
+
+const [pointer, setPointer] = createSignal(false);
+const [target, setTarget] = createSignal(null);
+
+window.addEventListener("mousedown", () => setPointer(true));
+window.addEventListener("mouseup", () => setPointer(false));
+window.addEventListener("mousemove", (e) => setTarget(e.target));
 
 function App() {
-  const lines:Array<Array<string>> = ["0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),
-                                      "0".repeat(10).split(''),]
-  
+  const lines = Array.from({ length: 10 }, () => Array(10).fill("0"));
   const [color, setColor] = createSignal(0);
 
-  function changeColor(getColor:any) {
-    let colorValue = getColor();
-    switch (colorValue){
-      case 1:
-        return "red";
-      case 2:
-        return "orange";
-      case 3:
-        return "yellow";
-      case 4:
-        return "green";
-      case 5:
-        return "cyan";
-      case 6:
-        return "blue";
-      case 7:
-        return "purple";
-      case 8:
-        return "black";
-      case 9:
-        return "gray";
-      default:
-        return "white";
-    }
-  }
+  const colorMapping = [
+    "white", "red", "orange", "yellow", "green", "cyan", "blue", "purple", "black", "gray"
+  ];
+
+  const changeColor = (index:number) => colorMapping[index] || "white";
+
+  const [tiles, setTiles] = createSignal(Array.from({ length: 10 }, () => Array(10).fill("0")));
+
+  const handleTileClick = (rowIndex:number, colIndex:number) => {
+    const newTiles = [...tiles()];
+    newTiles[rowIndex][colIndex] = color();
+    setTiles(newTiles);
+  };
 
   return (
     <>
-      <div id='background'>
-
-        <div id='pallette'>
-          <For each={"0".repeat(10).split('')}>
-            {(i, index) => {
-              let stylePrefab = color() == index()?"aha":"booo";
-              return(<div class={'tile '+changeColor(()=>index())}
-              on:click={() => {setColor(index())}}
-              style={"border: "+ (color() == index()?"0.15em solid #f8f800; width: 2.25em; height: 2.25em;":"none")}
+      <div id="background">
+        <div id="pallette">
+          <For each={Array.from({ length: 10 })}>
+            {(item, index) => (
+              <div
+                class={`tile ${changeColor(index())}`}
+                onClick={() => setColor(index())}
+                style={{
+                  border: color() === index() ? "0.15em solid #f8f800" : "none",
+                  width: "2.25em",
+                  height: "2.25em",
+                }}
               >
                 {index()}
-              </div>)
-            }}
-
+              </div>
+            )}
           </For>
         </div>
 
-        <div id='table'>
+        <div id="table">
           <For each={lines}>
-          {(line, index) => (
-            <div class='lin' id={"lin"+index()}>
-              <For each={line}>
-              {(item, innerIndex) => {
-                const [tile, setTile] = createSignal("0");
-                return(
-                <div class={'tile '+(tile())} 
-                id={"tile_"+index().toString()+innerIndex().toString()}
-                on:click={() => {setTile(changeColor(color)); console.log(tile(),index().toString()+innerIndex().toString())}}
-                >
-
-                </div>
-                )
-              }}
-              </For>
-            </div>
-          )}
+            {(line, rowIndex) => (
+              <div class="lin" id={"lin" + rowIndex()}>
+                <For each={line}>
+                  {(item, colIndex) => {
+                    const tileColor = tiles()[rowIndex()][colIndex()];
+                    return (
+                      <div
+                        class={`tile ${changeColor(tileColor)}`}
+                        id={`tile_${rowIndex()}_${colIndex()}`}
+                        onClick={() => handleTileClick(rowIndex(), colIndex())}
+                      ></div>
+                    );
+                  }}
+                </For>
+              </div>
+            )}
           </For>
         </div>
 
-        <div id='tools'>
-          <div id='btn-clear' on:click={()=>{
-            for(let i=0; i <= 99; i++){
-              let x = ""
-              if(i < 10) x = "0"+i;
-              else x = i.toString();
-              
-            }
-          }}
-          ><img class='tile' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAcLIQvtKnKctvtSvKxHQoksPMNW_2WJNX_w&s" alt="" /></div>
+        <div id="tools">
+          <div
+            id="btn-clear"
+            onClick={() => {
+              const newTiles = Array.from({ length: 10 }, () => Array(10).fill(color()));
+              setTiles(newTiles);
+            }}
+          >
+            <img
+              class="tile"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAcLIQvtKnKctvtSvKxHQoksPMNW_2WJNX_w&s"
+              alt="Clear"
+            />
+          </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
